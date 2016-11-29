2915bc68f7daa1b5ac69b31d39ca264426c1bdfd
andela-jare <jorg.are@andela.com>
2016-11-29 16:23:25 +0100

    feature(implement-jwt-signup): fix travis fails Finishes #134209515

--- config/env/all.js
+++ config/env/all.js
@@ -4,7 +4,7 @@ const rootPath = path.normalize(path.join(__dirname, '/../..'));
 
 module.exports = {
   root: rootPath,
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   db: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/cfh',
   secret: 'kissaSecret'
 };
